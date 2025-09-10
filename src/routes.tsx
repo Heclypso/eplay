4ab1546504e5dbfcf607d040602e6c27a3169043
Heclypso <heclypso@gmail.com>
2025-09-10 13:51:59 -0300

    #9 feat: desenvolvido o componente Hero

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Index'
 import Categories from './Pages/Categories/Index'
+import Product from './Pages/Product/index'
 
 const RoutesElement = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
