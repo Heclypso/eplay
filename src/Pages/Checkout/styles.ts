bfbf7d99aec816102e7c18590718fe240a804124
Heclypso <heclypso@gmail.com>
2025-09-15 12:41:13 -0300

    #20 feat: desenvolvido os cards de pagamento por cartão e por boleto do formulário da página de checkout.

--- src/Pages/Checkout/styles.ts
+++ src/Pages/Checkout/styles.ts
@@ -1,13 +1,29 @@
 import styled from 'styled-components'
 import { colors } from '../../styles'
 
-export const Row = styled.div`
+type RowProps = {
+  marginTop?: string
+}
+
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
+  align-items: flex-end;
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     font-size: 14px;
@@ -15,11 +31,30 @@ export const InputGroup = styled.div`
     margin-bottom: 8px;
   }
 
-  input {
+  input,
+  select {
     width: 100%;
     background-color: ${colors.white};
     border: 1px solid ${colors.white};
     height: 32px;
     padding: 0px 8px;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${colors.white};
+  background-color: ${(props) =>
+    props.isActive ? colors.green : colors.black};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0px 8px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
