08889bbd64dce0ac3ec7a4e81abacc2a450ab2ea
Heclypso <heclypso@gmail.com>
2025-09-15 10:54:45 -0300

    #19 feat: desenvolvido a página de checkout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Index'
 import Categories from './Pages/Categories/Index'
 import Product from './Pages/Product/index'
+import Checkout from './Pages/Checkout'
 
 const RoutesElement = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
     <Route path="/product/:id" element={<Product />} />
+    <Route path="/checkout" element={<Checkout />} />
   </Routes>
 )
 
