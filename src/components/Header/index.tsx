ec02db925b30f77720984ed6dde16f20bad0f9b1
Heclypso <heclypso@gmail.com>
2025-09-15 14:25:18 -0300

    #22 feat: desenvolvido a responsividade do menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { HeaderBar, CartButton, LinkItem, Links } from './styles'
+import { useDispatch, useSelector } from 'react-redux'
+
+import {
+  HeaderBar,
+  CartButton,
+  LinkItem,
+  Links,
+  Hamburger,
+  HeaderRow,
+  NavMobile
+} from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
-import { useDispatch, useSelector } from 'react-redux'
 
 import { open } from '../../store/reducers/cart'
 import { RootReducer } from '../../store'
@@ -11,6 +21,7 @@ import { RootReducer } from '../../store'
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -18,28 +29,48 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="Logo da EPLAY" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categories">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart} href="#">
-        {items.length} - Produto(s)
-        <img src={carrinho} alt="Ícone do carrinho de compras" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburger onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburger>
+          <Link to="/">
+            <img src={logo} alt="Logo da EPLAY" />
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to="/categories">Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart} href="#">
+          {items.length} <span>- Produto(s)</span>
+          <img src={carrinho} alt="Ícone do carrinho de compras" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
