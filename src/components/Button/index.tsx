1bf06b6ce89e8c6f73f5578c28d6aa4b25d6572b
Heclypso <heclypso@gmail.com>
2025-09-15 14:04:24 -0300

    #21 feat: desenvolvido a validação dos formulários

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonContainer, ButtonLink } from './styles'
 
 export type Props = {
-  type: 'button' | 'link'
+  type: 'button' | 'link' | 'submit'
   title: string
   to?: string
   onClick?: () => void
@@ -17,12 +17,12 @@ const Button = ({
   to,
   variant = 'primary'
 }: Props) => {
-  if (type === 'button') {
+  if (type === 'button' || type === 'submit') {
     return (
       <ButtonContainer
         variant={variant}
         title={title}
-        type="button"
+        type={type}
         onClick={onClick}
       >
         {children}
